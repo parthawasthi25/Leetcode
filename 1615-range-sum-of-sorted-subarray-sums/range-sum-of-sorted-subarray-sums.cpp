00fe1502888f9bf398bf584bf05f4466aac0c202
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2024-08-04 14:42:33 +0530

    Time: 92 ms (53.03%) | Memory: 27.1 MB (44.27%) - LeetSync

---
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<int>ss;
+        int Mod=1e9+7;
+        int cs;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            cs=nums[i];
+            ss.push_back(cs);
+                for(int j=i+1;j<n;j++){
+                    cs=nums[j]+cs;
+                    ss.push_back(cs);
+                }
+        }
+        sort(ss.begin(), ss.end());
+        sum = accumulate(ss.begin() + (left - 1), ss.begin() + right, 0, [&](int a, int b) {return (a + b) % Mod;});
+        return sum;  
+    }
+};
