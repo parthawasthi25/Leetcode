cb1c6a531379d0b3747dc56adcd93a0e7fbe20ee
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2024-08-05 15:04:46 +0530

    Time: 11 ms (90.72%) | Memory: 18.4 MB (31.70%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+    map<string, int> b;
+    int n = arr.size();
+    for (int i = 0; i < n; i++) {
+        b[arr[i]]++;
+    }
+    for (int i = 0; i < n; i++) {
+        if (b[arr[i]] == 1) {
+            k--;  
+            if (k == 0) {
+                return arr[i];
+            }
+        }
+    }return "";
+    }
+};
