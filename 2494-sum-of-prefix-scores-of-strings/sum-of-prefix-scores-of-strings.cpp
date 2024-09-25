bf3e70857c44e36bc1689a1e11b80d0d920dfb9f
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2024-09-25 09:15:34 +0530

    Time: 562 ms (69.57%) | Memory: 704 MB (71.07%) - LeetSync

---
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,47 @@
+class Trie {
+private:
+    Trie* children[26]{};
+    int cnt = 0;
+
+public:
+    void insert(string& w) {
+        Trie* node = this;
+        for (char c : w) {
+            int idx = c - 'a';
+            if (!node->children[idx]) {
+                node->children[idx] = new Trie();
+            }
+            node = node->children[idx];
+            ++node->cnt;
+        }
+    }
+
+    int search(string& w) {
+        Trie* node = this;
+        int ans = 0;
+        for (char c : w) {
+            int idx = c - 'a';
+            if (!node->children[idx]) {
+                return ans;
+            }
+            node = node->children[idx];
+            ans += node->cnt;
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        Trie* trie = new Trie();
+        for (auto& w : words) {
+            trie->insert(w);
+        }
+        vector<int> ans;
+        for (auto& w : words) {
+            ans.push_back(trie->search(w));
+        }
+        return ans;
+    }
+};
