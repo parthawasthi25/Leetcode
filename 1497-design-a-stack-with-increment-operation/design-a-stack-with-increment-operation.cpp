9b16d7c9b27d57212a5c4caa4dc2b96c5873a529
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2024-09-30 16:13:22 +0530

    Time: 23 ms (86.72%) | Memory: 25.9 MB (66.84%) - LeetSync

---
+++ 1497-design-a-stack-with-increment-operation/design-a-stack-with-increment-operation.cpp
@@ -0,0 +1,37 @@
+class CustomStack {
+public:
+    CustomStack(int maxSize) {
+        stk.resize(maxSize);
+        add.resize(maxSize);
+        i = 0;
+    }
+
+    void push(int x) {
+        if (i < stk.size()) {
+            stk[i++] = x;
+        }
+    }
+
+    int pop() {
+        if (i <= 0) {
+            return -1;
+        }
+        int ans = stk[--i] + add[i];
+        if (i > 0) {
+            add[i - 1] += add[i];
+        }
+        add[i] = 0;
+        return ans;
+    }
+
+    void increment(int k, int val) {
+        if (i > 0) {
+            add[min(k, i) - 1] += val;
+        }
+    }
+
+private:
+    vector<int> stk;
+    vector<int> add;
+    int i;
+};
