8aea6f3385efc2d77b70f68e689493430c2db93d
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2024-11-04 15:03:49 +0530

    Time: 17 ms (81.20%) | Memory: 27.6 MB (96.77%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        string result;
+        for (int i = 0, cnt = 0; i < size(word); ++i) {
+            ++cnt;
+            if (cnt == 9 || (i + 1 == size(word) || word[i + 1] != word[i])) {
+                result.push_back('0' + cnt);
+                result.push_back(word[i]);
+                cnt = 0;
+            }
+        }
+        return result;
+    }
+};
