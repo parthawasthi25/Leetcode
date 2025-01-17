85098dda15bc4b10ac98f0cccff643833ec6aa24
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2025-01-17 10:29:24 +0530

    Time: 0 ms (100.00%) | Memory: 264.1 MB (28.04%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,12 +1,10 @@
-// Time:  O(n)
-// Space: O(1)
-
-// array
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        return accumulate(cbegin(derived), cend(derived), 0, [](const auto& total, const auto& x) {
-            return total ^ x;
-        }) == 0;
+        int s = 0;
+        for (int x : derived) {
+            s ^= x;
+        }
+        return s == 0;
     }
 };
