215666ce2f51ba1c375e494fa34cc8c95665c0fb
Parth Awasthi <114384177+parthawasthi25@users.noreply.github.com>
2025-02-04 10:43:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (93.45%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -4,9 +4,9 @@ class Solution {
         int UL = 1, LL = 1;
         for(int i = 1;i < nums.length;i++){
             if(nums[i] > nums [i-1]){
-                LL = UL + 1;
-            }else if(nums[i] < nums[i-1]){
                 UL = LL + 1;
+            }else if(nums[i] < nums[i-1]){
+                LL = UL + 1;
             }
         }
         return Math.max(UL, LL);
